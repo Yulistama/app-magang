617261702d98af5a752b1412e0672c6960ab25cc
Avin Riyan <avinriyant@gmail.com>
2022-07-31 22:35:45 +0700

    update fitur konten
    update fitur konten

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Magang;
 use App\Models\Mahasiswa;
 use App\Models\Perusahaan;
+use App\Models\Positions;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -154,9 +155,149 @@ class AdminController extends Controller
             // dd($perusahaan);
             $data['perusahaan'] = $perusahaan;
 
+            $job = Positions::where('id_perusahaan', $id)->orderByDesc('id')->get();
+            $data['jobs'] = $job;
+
             return view('pages.admin.konten.konten', $data);
         }catch(Exception $e){
             Log::error($e->getMessage().'AdminController:konten');
+            Session::flash('error', $e->getMessage());
+            return redirect()->back();
+        }
+    }
+
+    public function ubahperusahaan(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'about' => 'required',
+                'location' => 'required',
+                'category' => 'required',
+            ]);
+            if($validator->fails()) throw new Exception($validator->errors()->first());
+
+            Perusahaan::where('id_perusahaan', Session::get('user.id'))->update([
+                'name' => $request->name,
+                'about' => $request->about,
+                'location' => $request->location,
+                'category' => $request->category,
+                'address'       => $request->address,
+                'phone'       => $request->phone,
+                'email'       => $request->email,
+                'employee'       => $request->employee,
+                'project_url'       => $request->project_url,
+                'image'  => $request->image
+            ]);
+
+            Alert::success('Berhasil', 'Anda berhasil mengedit perusahaan');
+            return redirect()->back()->withInput();
+        } catch (Exception $e) {
+            Log::error($e->getMessage().'AdminController:ubahperusahaan');
+            Session::flash('error', $e->getMessage());
+            return redirect()->back();
+        }
+    }    
+
+    public function tambahjob(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'position' => 'required',
+                'date' => 'required',
+                'status' => 'required',
+            ]);
+            if($validator->fails()) throw new Exception($validator->errors()->first());
+
+            $id = Session::get('user.id');
+            if(!$id){
+                Alert::error('Error', 'Anda harus login terlebih dahulu!');
+            }else{
+                
+                $add_position                 = new Positions;                
+                $add_position->id_perusahaan  = $id;
+                $add_position->name           = $request->name;
+                $add_position->position       = $request->position;
+                $add_position->date           = $request->date;
+                $add_position->status         = $request->status;                
+                $add_position->jobdesc_1       = $request->jobdesc_1;
+                $add_position->jobdesc_2       = $request->jobdesc_2;
+                $add_position->jobdesc_3       = $request->jobdesc_3;
+                $add_position->jobdesc_4       = $request->jobdesc_4;
+                $add_position->jobdesc_5       = $request->jobdesc_5;
+                $add_position->requirements_1  = $request->requirements_1;
+                $add_position->requirements_2  = $request->requirements_2;
+                $add_position->requirements_3  = $request->requirements_3;
+                $add_position->requirements_4  = $request->requirements_4;
+                $add_position->requirements_5  = $request->requirements_5; 
+                $add_position->descriptions  = $request->descriptions; 
+                $add_position->save();
+                
+                Alert::success('Berhasil', 'Anda berhasil menambahkan job baru');
+            }
+               
+            return redirect()->back()->withInput();
+
+
+        } catch (Exception $e) {
+            Log::error($e->getMessage().'AdminController:tambahjob');
+            Session::flash('error', $e->getMessage());
+            return redirect()->back();
+        }
+    }
+
+    public function ubahjob(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'position' => 'required',
+                'date' => 'required',
+                'status' => 'required',
+            ]);
+            if($validator->fails()) throw new Exception($validator->errors()->first());
+
+            Positions::where('id', $request->id)->update([
+                'name' => $request->name,
+                'position' => $request->position,
+                'date' => $request->date,
+                'status' => $request->status,
+                'jobdesc_1'       => $request->jobdesc_1,
+                'jobdesc_2'       => $request->jobdesc_2,
+                'jobdesc_3'       => $request->jobdesc_3,
+                'jobdesc_4'       => $request->jobdesc_4,
+                'jobdesc_5'       => $request->jobdesc_5,
+                'requirements_1'  => $request->requirements_1,
+                'requirements_2'  => $request->requirements_2,
+                'requirements_3'  => $request->requirements_3,
+                'requirements_4'  => $request->requirements_4,
+                'requirements_5'  => $request->requirements_5, 
+                'descriptions'  => $request->descriptions
+            ]);
+
+            Alert::success('Berhasil', 'Anda berhasil mengedit job');
+            return redirect()->back()->withInput();
+        } catch (Exception $e) {
+            Log::error($e->getMessage().'AdminController:ubahjob');
+            Session::flash('error', $e->getMessage());
+            return redirect()->back();
+        }
+    }
+
+    public function hapusjob($id)
+    {
+        try {
+            $job = Positions::findOrFail($id);
+            if (!$job) {
+                Alert::error('Error', 'Job tidak ditemukan');             
+            }else {
+                $job->delete();
+                Alert::success('Berhasil', 'Anda berhasil menghapus job');
+            }            
+            return redirect()->back()->withInput();
+        } catch (Exception $e) {
+            Log::error($e->getMessage().'AdminController:hapusjob');
             Session::flash('error', $e->getMessage());
             return redirect()->back();
         }
--- resources/views/pages/admin/konten/modal/editKonten.blade.php
+++ resources/views/pages/admin/konten/modal/editKonten.blade.php
@@ -9,116 +9,118 @@
       <div class="modal-body">
         <div class="row">
           <div class="col-12 px-5" id="dataStatusEdit" style="display: '';">
-            <!-- Vertical Form -->
-            <form class="row g-3">
+            <!-- Vertical Form -->            
+              <form class="row g-3" id="update-item-form" method="POST" action="">
+              @csrf
               <div class="col-12">
                 <label for="name" class="form-label fw-bold">Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Name">
+                <input type="hidden" name="id" id="id">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
               </div>
               <div class="col-12">
                 <label for="position" class="form-label fw-bold">Position</label>
-                <input type="text" class="form-control" id="position" placeholder="Position">
+                <input type="text" class="form-control" id="position" name="position" placeholder="Position">
               </div>
               <div class="col-12">
                 <label for="date" class="form-label fw-bold">Date</label>
-                <input type="date" class="form-control" id="date">
+                <input type="date" class="form-control" id="date" name="date">
               </div>
               <div class="col-12">
                   <label for="status" class="form-label fw-bold">Status</label>
                   <div class="col-sm-12">
-                    <select class="form-select" aria-label="Default select example">
-                      <option selected>--Pilih Status--</option>
-                      <option value="1">Aktif</option>
-                      <option value="2">Non Aktif</option>
+                    <select class="form-select" aria-label="Default select example" id="status" name="status">
+                      <option selected disabled>--Pilih Status--</option>
+                      <option value="Aktif">Aktif</option>
+                      <option value="Tidak Aktif">Tidak Aktif</option>
                     </select>
                   </div>
               </div>
-            </form>
+            <!-- </form> -->
             <!-- Vertical Form -->
           </div>
           <div class="col-12 px-5" id="dataDescripsiEdit" style="display: none;">
             <div class="row">
               <div class="col-12">
-                <form class="row g-3 mt-2">
+                <!-- <form class="row g-3 mt-2"> -->
                     <div class="col-12">
                       <label for="position" class="form-label fw-bold">Job Descriptions</label>
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Jobdesc 1</label>
+                        <input type="text" class="form-control" id="jobdesc_1" name="jobdesc_1" placeholder="Your Name">
+                        <label for="jobdesc_1">Jobdesc 1</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Jobdesc 2</label>
+                        <input type="text" class="form-control" id="jobdesc_2" name="jobdesc_2" placeholder="Your Name">
+                        <label for="jobdesc_2">Jobdesc 2</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Jobdesc 3</label>
+                        <input type="text" class="form-control" id="jobdesc_3" name="jobdesc_3" placeholder="Your Name">
+                        <label for="jobdesc_3">Jobdesc 3</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Jobdesc 4</label>
+                        <input type="text" class="form-control" id="jobdesc_4" name="jobdesc_4"  placeholder="Your Name">
+                        <label for="jobdesc_4">Jobdesc 4</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Jobdesc 5</label>
+                        <input type="text" class="form-control" id="jobdesc_5" name="jobdesc_5" placeholder="Your Name">
+                        <label for="jobdesc_5">Jobdesc 5</label>
                       </div>
                     </div>
                   
-                  </form>
+                  <!-- </form> -->
                   <!-- Vertical Form -->
               </div>
               
               <div class="col-12 my-5">
-                <form class="row g-3">
+                <!-- <form class="row g-3"> -->
                     <div class="col-12">
                       <label for="position" class="form-label fw-bold">Requirements</label>
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Requirements 1</label>
+                        <input type="text" class="form-control" id="requirements_1" name="requirements_1" placeholder="Your Name">
+                        <label for="requirements_1">Requirements 1</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Requirements 2</label>
+                        <input type="text" class="form-control" id="requirements_2" name="requirements_2" placeholder="Your Name">
+                        <label for="requirements_2">Requirements 2</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Requirements 3</label>
+                        <input type="text" class="form-control" id="requirements_3" name="requirements_3" placeholder="Your Name">
+                        <label for="requirements_3">Requirements 3</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Requirements 4</label>
+                        <input type="text" class="form-control" id="requirements_4" name="requirements_4" placeholder="Your Name">
+                        <label for="requirements_4">Requirements 4</label>
                       </div>
                     </div>
                     <div class="col-12">
                       <div class="form-floating">
-                        <input type="text" class="form-control" id="floatingName" placeholder="Your Name">
-                        <label for="floatingName">Requirements 5</label>
+                        <input type="text" class="form-control" id="requirements_5" name="requirements_5" placeholder="Your Name">
+                        <label for="requirements_5">Requirements 5</label>
                       </div>
                     </div>
                   
-                  </form>
+                  <!-- </form> -->
                   <!-- Vertical Form -->
               </div>
 
               <div class="col-12">
-              <label for="position" class="form-label fw-bold">Descriptions</label>
+              <label for="descriptions" class="form-label fw-bold">Descriptions</label>
                 <div class="form-floating">
-                  <textarea class="form-control" placeholder="Address" id="floatingTextarea" style="height: 100px;"></textarea>
-                  <label for="floatingTextarea">Descriptions</label>
+                  <textarea class="form-control" placeholder="Descriptions" id="descriptions" name="descriptions" style="height: 100px;"></textarea>
+                  <label for="descriptions">Descriptions</label>
                 </div>
               </div>
             </div>
@@ -134,7 +136,8 @@
           </div>
           <div class="col-12" id="display-prev-edit" style="display: none;">
             <button type="button" class="btn btn-outline-secondary" id="btn-prev-edit">Sebelumnya</button>
-            <button type="button" class="btn btn-info text-white">Simpan</button>
+            <button type="submit" class="btn btn-info text-white">Simpan</button>
+            </form>
           </div>
         </div>
       </div>
