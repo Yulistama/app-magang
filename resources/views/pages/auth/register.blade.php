075d53f4000bd0365397e9a9f262107ae6376ab6
Yulistama <edo.yulistama@gmail.com>
2022-07-31 23:57:24 +0700

    update img mhs

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -18,6 +18,14 @@
                         @endif
                     </div>
                 @endif
+
+                @if (session('error') != null)
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        {{ session('error') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+                
                 <div class="mb-3 px-5">
                     <a style="text-decoration: none; color:black;" href="<?php echo e(route('pilihlogin')); ?>">
                         <i style="font-size:20px; cursor:pointer" class="fa my-4">&#xf060; </i>
